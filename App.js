22968e7fdaa9ac50afc0638a7c24c1ca538479fc
HaydenLiu <huangxunliu@gmail.com>
2020-09-11 23:16:35 +1000

    fetch,expo

--- App.js
+++ App.js
@@ -1,16 +1,50 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    inputValue: "",
+    list: [1, 2, 3],
+  };
+  componentDidMount = () => {
+    this.getListInfo()
+  };
+  getListInfo() {
+    fetch('http://www.abc.com/list.json')
+      .then((res) => res.json())
+      .then((res) => {
+       this.setState({
+         list:res.data.list
+       })
+      })
+      
+  }
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View>
+          {this.state.list.map((item, index) => {
+            return (
+              <Text style={styles.item} key={index}>
+                {item}
+              </Text>
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    textAlign: "center",
+    lineHeight: 30,
+  },
+});
